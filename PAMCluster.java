ad69cda9fc303a3b14b8fea3185479b8af7cf878
SpencerHanson <spencer.whanson@gmail.com>
2014-04-18 14:01:26 -0500

    pam and pamcluster
    added things to PAMCluster to help compute cost
    started the swap method in PAM

--- PAMCluster.java
+++ PAMCluster.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class PAMCluster extends Cluster {
   private Data medoid;
+  private Double cost;
   
   public PAMCluster(Data medoid) {
     super();
@@ -9,7 +10,25 @@ public class PAMCluster extends Cluster {
   }
   
   public Data getMedoid() {return this.medoid;}
+
+ 
+  public double getCost() {return this.cost;}
+  public void setCost(double cost) {this.cost = cost;}
+
+  public void computeCost() {
+    for (int i = 0; i < points.size(); i++) {
+      for (Iterator<String> stuff = points.get(i).getAttributes().keySet().iterator() ; stuff.hasNext();) {
+        String current = stuff.next();
+        cost += Math.abs(medoid.getAttribute(current) - points.get(i).getAttribute(current));
+      }
+    }
+  }
+  
 }
+  
+  
+
+
 
 
 

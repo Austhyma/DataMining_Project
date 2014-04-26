52722954b556df93d410b8a743e27b6ab5196dc7
Austhyma <austhyma@gmail.com>
2014-04-26 15:05:19 -0500

    Current Progress on OPTICS algorithm

--- Optics.java
+++ Optics.java
@@ -13,8 +13,10 @@ public class Optics {
   private int minPoints;
   //Computed/resultant fields
   private ArrayList<OpticsData> dataset = new ArrayList<OpticsData>();
+  private ArrayList<Cluster> clusters = new ArrayList<Cluster>();
+  private boolean euclidean;
   
-  public Optics (String filename, String[] attributeNames, String epsilon, String minPoints) throws IOException {
+  public Optics (String filename, String[] attributeNames, String epsilon, String minPoints, String euclidean) throws IOException {
     try {
       this.file = new BufferedReader(new FileReader(filename));
     }
@@ -24,6 +26,7 @@ public class Optics {
     this.attributeNames = attributeNames;
     this.epsilon = Double.parseDouble(epsilon);
     this.minPoints = Integer.parseInt(minPoints);
+    this.euclidean = euclidean.equals("true");
     initData();
   }
   
@@ -45,6 +48,7 @@ public class Optics {
       line = file.readLine();
       //System.out.println("Line: " + count++);
     }
+    computeOptics();
   }
   
   public LinkedList<Double> insertValue(LinkedList<Double> values, double value) {
@@ -64,9 +68,77 @@ public class Optics {
     return a;
   }
   
-  //java Optics <filename> <epsilon> <minPoints>
+  //Algorithm
+  public void computeOptics() {
+    int count = 0;
+    for (OpticsData point : dataset) {
+      if (point.processed()) continue;
+      ArrayList<Data> clusterPoints = new ArrayList<Data>();
+      ArrayList<OpticsData> neighbors = getNeighbors(point);
+      System.out.println("Neighbors: " + neighbors.size());
+      point.process();
+      clusterPoints.add(point);
+      ArrayList<OpticsData> seeds = new ArrayList<OpticsData>();
+      if (point.coreDistance(neighbors, this.minPoints, this.euclidean) != null) {
+        seeds = update(neighbors, point, seeds);
+        for (OpticsData q : seeds) {
+          ArrayList<OpticsData> qNeighbors = getNeighbors(q);
+          q.process();
+          clusterPoints.add(q);
+          if (q.coreDistance(qNeighbors, this.minPoints, this.euclidean) != null) {
+            seeds = update(qNeighbors, q, seeds);
+          }
+        }
+      }
+      System.out.println("Points processed: " + count++);
+      clusters.add(new Cluster(clusterPoints));
+    }
+  }
+  
+  public ArrayList<OpticsData> getNeighbors(OpticsData point) {
+    ArrayList<OpticsData> neighbors = new ArrayList<OpticsData>();
+    for (OpticsData data : dataset) {
+      double distance = point.distance(data, euclidean);
+      if (distance <= this.epsilon) {
+        neighbors.add(data);
+        data.setNeighborDistance(distance);
+      }
+    }
+    Collections.sort(neighbors, new Comparator<OpticsData>() {
+      public int compare(OpticsData data1, OpticsData data2) {
+        return data1.getNeighborDistance().compareTo(data2.getNeighborDistance());
+      }
+    });
+    return neighbors;
+  }
+  
+  public ArrayList<OpticsData> update(ArrayList<OpticsData> neighbors, OpticsData point, ArrayList<OpticsData> seeds) {
+    double coreDistance = point.getCoreDistance();
+    for (OpticsData neighbor : neighbors) {
+      if (!neighbor.processed()) {
+        double reachabilityDistance = Math.max(coreDistance, neighbor.getNeighborDistance());
+        if (neighbor.getReachabilityDistance() == null) {
+          neighbor.setReachabilityDistance(reachabilityDistance);
+          seeds.add(neighbor);
+        }
+        else {
+          if (reachabilityDistance < neighbor.getReachabilityDistance()) {
+            neighbor.setReachabilityDistance(reachabilityDistance);
+          }
+        }
+      }
+    }
+    Collections.sort(seeds, new Comparator<OpticsData>() {
+      public int compare(OpticsData data1, OpticsData data2) {
+        return data1.getReachabilityDistance().compareTo(data2.getReachabilityDistance());
+      }
+    });
+    return seeds;
+  }
+  
+  //java Optics <filename> <epsilon> <minPoints> <euclidean>
   public static void main(String[] args) throws IOException {
     String[] initAttNames = {"NCD", "AI", "AS(NA)", "BL", "NAC", "AS(NAC)", "CS", "AT", "NA", "ADL", "NAD"};
-    Optics init = new Optics(args[0], initAttNames, args[1], args[2]);
+    Optics init = new Optics(args[0], initAttNames, args[1], args[2], args[3]);
   }
 }
--- OpticsData.java
+++ OpticsData.java
@@ -3,13 +3,52 @@ import java.math.*;
 
 public class OpticsData extends Data {
   
-  private double reachabilityDistance = Double.POSITIVE_INFINITY;;
+  private Double reachabilityDistance;
+  private Double coreDistance;
+  private boolean processed = false;
+  private Double neighborDistance;
   
+  //Default Constructor
   public OpticsData() {
     super();
   }
   
+  //Constructor
   public OpticsData(HashMap<String, Double> attributes, boolean buzz) {
     super(attributes, buzz);
+  }
+  
+  //Getters and Setters
+  public void setNeighborDistance(Double neighborDistance) {this.neighborDistance = neighborDistance;}
+  public Double getNeighborDistance() {return this.neighborDistance;}
+  public Double getCoreDistance() {return this.coreDistance;}
+  public Double getReachabilityDistance() {return this.reachabilityDistance;}
+  public void setReachabilityDistance(double reachabilityDistance) {this.reachabilityDistance = reachabilityDistance;}
+  public void process() {this.processed = true;}
+  public boolean processed() {return this.processed;}
+  
+  //Calculates distance between two points
+  //TODO: Needs to weight the distances
+  public double distance(OpticsData other, boolean euclidean) {
+    double distance = 0;
+    for (Iterator<String> attribute = this.attributes.keySet().iterator(); attribute.hasNext();) {
+      String current = attribute.next();
+      double manValue = Math.abs(other.getAttribute(current) - this.attributes.get(current));
+      distance += (euclidean) ? Math.pow(manValue, 2) : manValue;
+    }
+    return distance;
+  }
+  
+  //Calculates the core-distance for each point from it's minPointsith neighbor
+  public Double coreDistance(ArrayList<OpticsData> neighbors, int minPoints, boolean euclidean) {
+    Double coreDistance;
+    if (neighbors.size() < minPoints) {
+      return null;
+    }
+    else {
+      coreDistance = distance(neighbors.get(minPoints-1), euclidean);
+      this.coreDistance = coreDistance;
+    }
+    return coreDistance;
   }
 }
